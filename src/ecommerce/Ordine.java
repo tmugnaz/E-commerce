817d37fc8f3d1983bb45fc4ef999b81bc257172f
tmugnai <tmugnai@mail.com>
2024-05-06 09:13:40 +0200

    fix errori

--- src/ecommerce/Ordine.java
+++ src/ecommerce/Ordine.java
@@ -13,39 +13,44 @@ public class Ordine {
 	private String pagamentoSelezionato;
 	private List<RigaOrdine> righeOrdine = new ArrayList<RigaOrdine>();
 
-	public Ordine(Utente utente,String nome,Scanner scanner) {
-		this.nome= nome;
+	public Ordine(Utente utente, String nome, Scanner scanner) {
+		this.nome = nome;
 		this.utente = utente;
 		addInitialModPagamento();
 		sceltaModPagScanner(scanner);
 		setIndirizzi(utente);
-		this.stato= STATO.CREATO;
+		this.stato = STATO.CREATO;
 	}
-	public Ordine (Utente utente,String nome) {
-		this.nome=nome;
-		this.utente=utente;
+
+	public Ordine(Utente utente, String nome) {
+		this.nome = nome;
+		this.utente = utente;
 		setIndirizzi(utente);
-		this.stato= STATO.CREATO;
+		this.stato = STATO.CREATO;
 	}
-	public Ordine(Utente utente , String nome, String modPagamento) {
-		this.nome=nome;
-		this.utente=utente;
+
+	public Ordine(Utente utente, String nome, String modPagamento) {
+		this.nome = nome;
+		this.utente = utente;
 		this.addInitialModPagamento();
-		
-		this.pagamentoSelezionato= modPagamento;
+
+		this.checkModalit‡Pagamento(modPagamento);
 		setIndirizzi(utente);
-		this.stato= STATO.CREATO;
-		
+		this.stato = STATO.CREATO;
+
 	}
+
 	public void pagaOrdine() {
 		this.stato = STATO.PAGATO;
 	}
+
 	public void spedisciOrdine() {
-		this.stato= STATO.SPEDITO;
-		
+		this.stato = STATO.SPEDITO;
+
 	}
+
 	public void consegnaOrdine() {
-		this.stato=STATO.CONSEGNATO;
+		this.stato = STATO.CONSEGNATO;
 	}
 
 	public void setIndirizzi(Utente utente) {
@@ -60,10 +65,10 @@ public class Ordine {
 			for (IndirizzoUtente in : utente.getIndirizzi()) {
 				if (in.getIndirizzotype() == INDIRIZZOTYPE.FATTURAZIONE) {
 					this.indirizzoFatturazione = in.getIndirizzoVia().concat(in.getCAP());
-					
+
 				} else if (in.getIndirizzotype() == INDIRIZZOTYPE.SPEDIZIONE) {
 					this.indirizzoSpedizione = in.getIndirizzoVia().concat(in.getCAP());
-					
+
 				}
 			}
 		}
@@ -74,94 +79,89 @@ public class Ordine {
 		getModalit‡Pagamento().add("Carta di credito");
 		getModalit‡Pagamento().add("Paypal");
 	}
+
 	public void info() {
-		System.out.println("Ordine :"+getNome()+" Utente: "+getUtente().getCognome()+" "+getUtente().getCognome()+" Indirizzo Spedizione: "+getIndirizzoSpedizione()
-							+" Indirizzo Fatturazione: "+getIndirizzoFatturazione()+" modalit‡ pagamento selezionata: "+getPagamentoSelezionato()
-							+" Stato ordine: "+getStato().name()+";");
+		System.out.println("Ordine :" + getNome() + " Utente: " + getUtente().getCognome() + " "
+				+ getUtente().getCognome() + " Indirizzo Spedizione: " + getIndirizzoSpedizione()
+				+ " Indirizzo Fatturazione: " + getIndirizzoFatturazione() + " modalit‡ pagamento selezionata: "
+				+ getPagamentoSelezionato() + " Stato ordine: " + getStato().name() + ";");
 		stampaRigheOrdine(this);
 	}
 
-	
 	public void stampaRigheOrdine(Ordine o) {
 		for (RigaOrdine rg : o.getRigheOrdine()) {
 			rg.info();
 		}
 	}
 
-
-	public void creaRigaOrdine (Articolo articolo, int quantit‡, int iva, int sconto) {
-		RigaOrdine rg = new RigaOrdine (this,articolo,quantit‡,iva,sconto);
+	public void creaRigaOrdine(Articolo articolo, int quantit‡, int iva, int sconto) {
+		RigaOrdine rg = new RigaOrdine(this, articolo, quantit‡, iva, sconto);
 		addRigaOrdine(rg);
-		
+
 	}
-	
-	
-	public void addRigaOrdine( RigaOrdine rg ) {
+
+	public void addRigaOrdine(RigaOrdine rg) {
 		this.righeOrdine.add(rg);
 	}
-	
+
 	public void checkModalit‡Pagamento(String candidato) {
 		for (String string : modalit‡Pagamento) {
 			if (candidato == string) {
 				this.setPagamentoSelezionato(candidato);
-			}else {
+			} else {
 				this.modalit‡Pagamento.add(candidato);
 				this.setPagamentoSelezionato(candidato);
 			}
-			
+
 		}
 	}
-	
+
 	// METODI SCANNER PER INTERFACCIA UTENTE, SCELTA DI METODO DI PAGAMENTO
 	public void sceltaModPagScanner(Scanner scanner) {
 		int cont = 0;
-			System.out.println("Modalit‡ pagamento correnti");
-			for (String string : this.modalit‡Pagamento) {
+		System.out.println("Modalit‡ pagamento correnti");
+		for (String string : this.modalit‡Pagamento) {
 
-				System.out.println(string + " " + cont);
-				cont++;
-			}
-			System.out.println(
-					"Selezionare la modalit‡ di pagamento, inserire \"3\" se si desidera inserire una nuova modalit‡");
-			int sceltaUtente = scanner.nextInt();
-			System.out.println();
-			switch (sceltaUtente) {
-			case (0): {
-				System.out.println("hai scelto Bonfifico Bancario");
-				this.setPagamentoSelezionato(this.modalit‡Pagamento.get(0));
-				break;
-			}
-			case (1): {
-				System.out.println("hai scelto Carta di Credito");
-				this.setPagamentoSelezionato(this.modalit‡Pagamento.get(1));
-				break;
-			}
-			case (2): {
-				System.out.println("hai scelto Paypal");
-				this.setPagamentoSelezionato(this.modalit‡Pagamento.get(2));
-				break;
-			}
-			case (3): {
-				System.out.println("Prego inserire metodo di pagamento alternativo");
-				String metodoAlt = scanner.next();
-				System.out.println();
-				this.setPagamentoSelezionato(metodoAlt);
-				break;
-			}
-			default: {
-				System.out.println("scelta non valida ordine annullato");
-				break;
-			}
-			}
-
+			System.out.println(string + " " + cont);
+			cont++;
 		}
-	
-	
-	
+		System.out.println(
+				"Selezionare la modalit‡ di pagamento, inserire \"3\" se si desidera inserire una nuova modalit‡");
+		int sceltaUtente = scanner.nextInt();
+		System.out.println();
+		switch (sceltaUtente) {
+		case (0): {
+			System.out.println("hai scelto Bonfifico Bancario");
+			this.setPagamentoSelezionato(this.modalit‡Pagamento.get(0));
+			break;
+		}
+		case (1): {
+			System.out.println("hai scelto Carta di Credito");
+			this.setPagamentoSelezionato(this.modalit‡Pagamento.get(1));
+			break;
+		}
+		case (2): {
+			System.out.println("hai scelto Paypal");
+			this.setPagamentoSelezionato(this.modalit‡Pagamento.get(2));
+			break;
+		}
+		case (3): {
+			System.out.println("Prego inserire metodo di pagamento alternativo");
+			String metodoAlt = scanner.next();
+			System.out.println();
+			this.setPagamentoSelezionato(metodoAlt);
+			break;
+		}
+		default: {
+			System.out.println("scelta non valida ordine annullato");
+			break;
+		}
+		}
+
+	}
+
 	/// GETTER E SETTER --------------------------------------------
 
-	
-	
 	public void setPagamentoSelezionato(String scelta) {
 		this.pagamentoSelezionato = scelta;
 
@@ -170,15 +170,19 @@ public class Ordine {
 	public List<String> getModalit‡Pagamento() {
 		return modalit‡Pagamento;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public void setModalit‡Pagamento(List<String> modalit‡Pagamento) {
 		this.modalit‡Pagamento = modalit‡Pagamento;
 	}
+
 	public Utente getUtente() {
 		return utente;
 	}
